0830a43f980aea0d91355ff9b414140c5517aa49
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-01-25 17:56:59 +0600

    errorpage

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -7,7 +7,7 @@ const Error = () => {
     return (
         <div className='max-w-6xl mx-auto flex justify-center items-center flex-col mt-[220px] space-y-3 p-10'>
             <Helmet>
-                <title>BlogSpace | Error</title>
+                <title>BlogSpace | ErrorPage</title>
             </Helmet>
             <h3 className='text-3xl text-red-500 text-center font-bold'>PAGE NOT FOUND</h3>
             <p className='text-3xl text-red-500 text-center font-bold'>ERROR 404 STATUS</p>
