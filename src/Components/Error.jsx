5672abc1b908a0139b425c59173799e90d43a1c2
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-01-25 17:55:38 +0600

    error page add

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,10 +1,17 @@
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
+import '../Components/index/index.css';
 
 
 const Error = () => {
     return (
-        <div>
-            error page
-            404 status
+        <div className='max-w-6xl mx-auto flex justify-center items-center flex-col mt-[220px] space-y-3 p-10'>
+            <Helmet>
+                <title>BlogSpace | Error</title>
+            </Helmet>
+            <h3 className='text-3xl text-red-500 text-center font-bold'>PAGE NOT FOUND</h3>
+            <p className='text-3xl text-red-500 text-center font-bold'>ERROR 404 STATUS</p>
+            <Link to="/" className='btn bg-white text-red-500 adminColor'>Back to Home</Link>
         </div>
     );
 };
